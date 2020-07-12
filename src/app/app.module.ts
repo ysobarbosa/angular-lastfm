968b459218e27d6bc2261a4b2f61da5f6e5de78e
Yasmin <ysobarbosa@gmail.com>
2020-07-12 20:13:42 -0300

    chamando a api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApisService } from './service/apis.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from  '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { AlbunsComponent } from './component/albuns/albuns.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbunsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule, 
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApisService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
